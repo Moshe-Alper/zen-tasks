00a2166a3e7754f18d43a0e23cf88e71ba08c50e
Moshik Alper <moshikalper@gmail.com>
2025-05-26 19:22:00 +0300

    Enhance user selection and task completion features

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { AppHeaderComponent } from './cmps/app-header/app-header.component';
 import { UserComponent } from './cmps/user/user.component';
 import { DUMMY_USERS } from './data/dummy-users';
-import { TasksComponent } from "./cmps/tasks/tasks.component";
+import { TasksComponent } from './cmps/tasks/tasks.component';
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -11,14 +11,14 @@ import { TasksComponent } from "./cmps/tasks/tasks.component";
   styleUrl: './app.component.css',
 })
 export class AppComponent {
-  users = DUMMY_USERS
-  selectedUserId?: string
+  users = DUMMY_USERS;
+  selectedUserId?: string;
 
   get selectedUser() {
-    return this.users.find((user) => user.id === this.selectedUserId)!
+    return this.users.find((user) => user.id === this.selectedUserId);
   }
 
   onSelectUser(id: string) {
-    this.selectedUserId = id
+    this.selectedUserId = id;
   }
 }
--- src/app/cmps/user/user.component.ts
+++ src/app/cmps/user/user.component.ts
@@ -9,8 +9,8 @@ import { User } from '../../models/user.model';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-
   @Input({required: true}) user!: User
+  @Input({required: true}) selected!: boolean
   @Output() select = new EventEmitter<string>()
 
   get imagePath() {
