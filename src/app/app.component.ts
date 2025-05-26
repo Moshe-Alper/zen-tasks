9e3380c77e64bef24e5abf1ad684aeaacd189e5e
Moshik Alper <moshikalper@gmail.com>
2025-05-26 18:13:04 +0300

    Refactor user and tasks components for improved user selection and display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,23 @@ import { Component } from '@angular/core';
 import { AppHeaderComponent } from './cmps/app-header/app-header.component';
 import { UserComponent } from './cmps/user/user.component';
 import { DUMMY_USERS } from './data/dummy-users';
+import { TasksComponent } from "./cmps/tasks/tasks.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AppHeaderComponent, UserComponent],
+  imports: [AppHeaderComponent, UserComponent, TasksComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 export class AppComponent {
   users = DUMMY_USERS
+  selectedUserId = 'u1'
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id
+  }
 }
--- src/app/cmps/user/user.component.ts
+++ src/app/cmps/user/user.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, input, Input, Output } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'user',
@@ -8,13 +9,15 @@ import { Component, Input } from '@angular/core';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({required: true}) avatar!: string
-  @Input({required: true}) name!: string
+
+  @Input({required: true}) user!: User
+  @Output() select = new EventEmitter<string>()
 
   get imagePath() {
-    return 'assets/users/' + this.avatar
+    return 'assets/users/' + this.user.avatar
   }
 
   onSelectUser() {
+    this.select.emit(this.user.id)
   }
 }
