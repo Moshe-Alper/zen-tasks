0531fb3c06e9af9710f346609a592fc204dfe009
Moshik Alper <moshikalper@gmail.com>
2025-05-26 16:16:26 +0300

    Add user cmp and style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
-import { AppHeaderComponent } from "./cmps/app-header/app-header.component";
-
+import { AppHeaderComponent } from './cmps/app-header/app-header.component';
+import { UserComponent } from './cmps/user/user.component';
+import { DUMMY_USERS } from './data/dummy-users';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [AppHeaderComponent],
+  imports: [AppHeaderComponent, UserComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {}
+export class AppComponent {
+  users = DUMMY_USERS
+}
