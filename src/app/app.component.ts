9e431feb7f22db0ade5f5e316b76a0fe0edcb845
Moshik Alper <moshikalper@gmail.com>
2025-05-26 15:10:33 +0300

    Add header and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { AppHeaderComponent } from "./cmps/app-header/app-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [],
+  imports: [AppHeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
