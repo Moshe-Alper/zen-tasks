6a99581577bfa75eb4fb9e03918039a1d2a7db2f
Moshik Alper <moshikalper@gmail.com>
2025-05-26 19:33:18 +0300

    Implemented AddTaskComponent with HTML and CSS for task creation

--- src/app/cmps/tasks/tasks.component.ts
+++ src/app/cmps/tasks/tasks.component.ts
@@ -1,23 +1,29 @@
 import { Component, Input } from '@angular/core';
-import { TaskPreviewComponent } from "../task-preview/task-preview.component";
-import { dummyTasks } from './../../data/dummy-tasks'
+import { TaskPreviewComponent } from '../task-preview/task-preview.component';
+import { dummyTasks } from './../../data/dummy-tasks';
+import { AddTaskComponent } from "../add-task/add-task.component";
 @Component({
   selector: 'tasks',
   standalone: true,
-  imports: [TaskPreviewComponent],
+  imports: [TaskPreviewComponent, AddTaskComponent],
   templateUrl: './tasks.component.html',
-  styleUrl: './tasks.component.css'
+  styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({required: true}) userId!: string 
-  @Input({required: true}) name!: string 
-  tasks = dummyTasks
+  @Input({ required: true }) userId!: string;
+  @Input({ required: true }) name!: string;
+  isAddingTask = false;
+  tasks = dummyTasks;
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId)
+    return this.tasks.filter((task) => task.userId === this.userId);
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id )
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  onStartAddTask() {
+    this.isAddingTask = true;
   }
 }
