5ce738c33eb7f68fdca8c7c590b9a0436f8637f2
Moshik Alper <moshikalper@gmail.com>
2025-05-27 17:10:42 +0300

    Refactor AddTaskComponent to use TasksService for task management and update task preview date format

--- src/app/cmps/tasks/tasks.component.ts
+++ src/app/cmps/tasks/tasks.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
-import { TaskPreviewComponent } from '../task-preview/task-preview.component';
-import { dummyTasks } from './../../data/dummy-tasks';
-import { AddTaskComponent } from "../add-task/add-task.component";
-import { NewTaskData } from '../../models/task.model';
+import { Component, Input } from '@angular/core'
+import { TaskPreviewComponent } from '../task-preview/task-preview.component'
+import { dummyTasks } from './../../data/dummy-tasks'
+import { AddTaskComponent } from "../add-task/add-task.component"
+import { NewTaskData } from '../../models/task.model'
+import { TasksService } from '../../services/tasks.service'
 @Component({
   selector: 'tasks',
   standalone: true,
@@ -11,35 +12,26 @@ import { NewTaskData } from '../../models/task.model';
   styleUrl: './tasks.component.css',
 })
 export class TasksComponent {
-  @Input({ required: true }) userId!: string;
-  @Input({ required: true }) name!: string;
-  isAddingTask = false;
-  tasks = dummyTasks;
+  @Input({ required: true }) userId!: string
+  @Input({ required: true }) name!: string
+  isAddingTask = false
+
+
+  constructor(private tasksService: TasksService) {}
 
   get selectedUserTasks() {
-    return this.tasks.filter((task) => task.userId === this.userId);
+    return this.tasksService.getUserTasks(this.userId)
   }
 
   onCompleteTask(id: string) {
-    this.tasks = this.tasks.filter((task) => task.id !== id);
+    
   }
 
   onStartAddTask() {
-    this.isAddingTask = true;
+    this.isAddingTask = true
   }
 
-  onCancelAddTask() {
-    this.isAddingTask = false
-  }
-  
-  onAddTask(taskData: NewTaskData) {
-    this.tasks.unshift({
-      id: new Date().getTime().toString(),
-      userId: this.userId,
-      title: taskData.title,
-      summary: taskData.summary,
-      dueDate: taskData.date
-    })
+  onCloseAddTask() {
     this.isAddingTask = false
   }
 }
---
+++ src/app/services/tasks.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@angular/core';
+import { dummyTasks } from '../data/dummy-tasks';
+import { NewTaskData } from '../models/task.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TasksService {
+  constructor() {}
+
+  private tasks = dummyTasks
+
+  getUserTasks(userId: string) {
+    return this.tasks.filter((task) => task.userId === userId)
+  }
+
+  addTask(taskData: NewTaskData, userId: string) {
+      this.tasks.unshift({
+      id: new Date().getTime().toString(),
+      userId: userId,
+      title: taskData.title,
+      summary: taskData.summary,
+      dueDate: taskData.date
+    })
+  }
+
+  removeTask(id: string) {
+    this.tasks = this.tasks.filter((task) => task.id !== id)
+  }
+
+
+}
